987f2edae54e5d8ea60e730d560bd01fb197cb89
Renuka <renu.bagave1901@gmail.com>
2024-04-23 12:11:27 +0530

    [Renuka]Add : Added Fibonacci program with recursion

---
+++ Logic_Programs/Fibonacci2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_Programs
+{
+    public class Fibonacci2
+    {
+        int no1, no2, no3, count;
+
+        public Fibonacci2()
+        {
+            no1 = no3 = 0;
+            no2 = 1;
+        }
+
+        public void FibonacciSeries2()
+        {
+            Console.WriteLine("Fibonacci with Recursion");
+            Console.WriteLine("Till what count you want?");
+            count = Convert.ToInt32(Console.ReadLine());
+            Console.Write(no1 + " " + no2);
+            Countseries(count - 2);
+
+        }
+
+        public void Countseries(int count)
+        {
+            if (count > 0)
+            {
+                no3 = no1 + no2;
+                Console.Write(" " + no3);
+                no1 = no2;
+                no2 = no3;
+                Countseries(count - 1);
+            }
+        }
+    }
+}
--- Logic_Programs/Program.cs
+++ Logic_Programs/Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Fibonacci f1 = new Fibonacci();
-            f1.FibonacciSeries();
+            /*Fibonacci f1 = new Fibonacci();
+            f1.FibonacciSeries();*/
+
+            Fibonacci2 f2 = new Fibonacci2();
+            f2.FibonacciSeries2();
+
         }
     }
 }
