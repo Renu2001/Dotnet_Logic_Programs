3e7c425b769050b7368fbeb48233a13e30d26b61
Renuka <renu.bagave1901@gmail.com>
2024-04-23 11:44:34 +0530

    [Renuka]Add : Added Fibonacci program

---
+++ Logic_Programs/Fibonacci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_Programs
+{
+    public class Fibonacci
+    {
+        int no1, no2, no3, count;
+
+        public Fibonacci()
+        {
+            no1 = 0;
+            no2 = 1;
+        }
+
+        public void FibonacciSeries()
+        {
+            Console.WriteLine("Fibonacci Series Problem \n");
+            Console.WriteLine("Till what do you want the series?");
+            count = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Fibonacci series is : " + no1 + " " + no2);
+
+            for (int i = 2; i < count; ++i)
+            {
+                no3 = no1 + no2;
+                Console.Write(" " + no3);
+                no1 = no2;
+                no2 = no3;
+
+            }
+        }
+
+
+    }
+}
---
+++ Logic_Programs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Logic_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci f1 = new Fibonacci();
+            f1.FibonacciSeries();
+        }
+    }
+}
